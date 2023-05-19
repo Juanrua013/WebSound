748c6c35e9d39e625547ca46091350b973292538
Juanrua013 <87742070+Juanrua013@users.noreply.github.com>
2023-05-18 19:09:23 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,11 +9,14 @@ function preload() {
 function setup () {
   createCanvas(300,300) ;
   slider= createSlider(0, 1, 0.5 , 0.01 ); 
+   
   song.play();
   song.setVolume(slider.value()); 
+  song.loop();
 }
 
 function draw () {
   background(0);
+  circle(100, 100);
   song.setVolume(slider.value());  
 }
