549b5aef5ff33e2d019cb7f9dc7967470573034e
Juanrua013 <87742070+Juanrua013@users.noreply.github.com>
2023-05-26 19:49:31 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,29 +2,30 @@ var song ;
 var slider ; 
 var sliderPan ; 
 var button;    
-var jumpButton ;  
+var jumpButton ;
+var amp;    
 
 function setup () {
   createCanvas(500,500) ;
 
   slider= createSlider(0, 1, 0.5 , 0.01 ); 
   sliderPan= createSlider(-1, 1, 0.5 , 0.01 ); 
-  button=createButton("play"); 
-  button.mousePressed(togglePlaying);
-
-  jumpButton = createButton("jump");  
-  jumpButton.mousePressed(jumpSong);  
+ 
   song.setVolume(slider.value()); 
-
+  amp = new p5.Amplitude();  
   
 }
 
 function draw () {
- // background(0);
-  song.addCue(1 , changeBackground,  color(255,0,255)); 
-  song.addCue(2 , changeBackground,  color(255,0,255));
-  song.addCue(3 , changeBackground,  color(0,255,255));
-  ellipse(56, 46, 55, 55);
+  background(51);
+
+
+  var vol = amp.getLevel();   
+  var diam = map( vol, 0 , 0.3 , 10 , 200); 
+
+  fill (255); 
+  stroke(100, 100, 100); 
+  ellipse(width/2, height/2 ,diam, diam);
   
   song.setVolume(slider.value());  
   song.pan(sliderPan.value());   
@@ -44,7 +45,7 @@ function togglePlaying(){
 }
 
 function jumpSong() { 
-    var len = song.duration() ;
+    var len = song.duration() ; 
     var t = random(len);  
     console.log (t);    
     song.jump(t) ; 
@@ -55,11 +56,12 @@ function jumpSong() {
 function preload() {
   soundFormats("mp3")
   song =  loadSound("./beat");
+  button=createButton("play"); 
+  button.mousePressed(togglePlaying);
+
+  jumpButton = createButton("jump");  
+  jumpButton.mousePressed(jumpSong); 
 }
 
-function changeBackground (){ 
 
-  background(random (255),  random(255),  random(255) );  
-
-}
   
